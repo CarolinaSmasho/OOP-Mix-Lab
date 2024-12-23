e452350fff9acb215aa8a7924da812d9f9c8bb54
CarolinaSmasho <lzbix20134@gmail.com>
2024-12-23 11:06:49 +0700

    Update item1_2.py

--- Lab 2/item1_2.py
+++ Lab 2/item1_2.py
@@ -1,10 +1,3 @@
-def day_of_year(day, mont, year):
-    return 0
-
-
-
-
-
 def is_leap(year):
     year = int(year)
     if year % 4 == 0:
@@ -12,7 +5,26 @@ def is_leap(year):
     else:
         return False
 
+def day_of_year(day, month, year):
+    day = int(day)
+    month = int(month)
+    year = int(year)
+    day = 0
 
-value = input()
-output = is_leap(value)
+    
+
+    day_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+    for x in range(month-1):
+        # print(day_in_month[x])
+        day += day_in_month[x]
+    return day
+
+
+
+
+
+# value = input()
+value = ("15/13/2024")
+value =[str(e) for e in value.split("/")]
+output = day_of_year(value[0],value[1],value[2])
 print(output)
