234ce2252d64be6e08223479af0f7ecb7f83e37c
CarolinaSmasho <lzbix20134@gmail.com>
2024-12-25 01:38:23 +0700

    Lab3 TestCase1 Pass

---
+++ Lab 3/main.py
@@ -0,0 +1,248 @@
+class Student:
+    def __init__(self, id, name):
+        self.__student_id = id
+        self.__student_name = name
+        
+    def get_student_name(self):
+        return self.__student_name
+    
+    def get_student_id(self):
+        return self.__student_id
+
+    def __str__(self):
+        return "Student: " + self.__student_name
+
+
+class Subject:
+    def __init__(self, id, name, credit):
+        self.__subject_id = id
+        self.__subject_name = name
+        self.__credit = credit
+        self.__subject_teacher= ""
+        
+    def get_subject_id(self):
+        return self.__subject_id
+    
+    def get_subject_name(self):
+        return self.__subject_name
+    
+    def assign_teacher(self, teacher):
+        self.__subject_teacher = teacher
+    
+    subject_id = property(get_subject_id)
+
+    def __str__(self):
+        return "My Class: " + self.__subject_name
+    
+class Teacher:
+    def __init__(self, id, name):
+        self.__teacher_id = id
+        self.__teacher_name = name
+
+class Enrollment:
+    def __init__(self, subject, student):
+        self.__enrollment_student = student
+        self.__enrollment_subject = subject
+    def get_student(self):
+        return self.__enrollment_student
+    def get_subject(self):
+        return self.__enrollment_subject
+    def __str__(self):
+        return "Enroll: " + self.__enrollment_student.__str__() + " in " + self.__enrollment_subject.__str__()
+
+
+
+student_list = []
+subject_list = []
+teacher_list = []
+enrollment_list = []
+
+# TODO 1 : function สำหรับค้นหา instance ของวิชาใน subject_list
+def search_subject_by_id(subject_id):
+    for x in subject_list:
+        if x.subject_id == subject_id:
+            return x
+    else:
+        return None
+
+# TODO 2 : function สำหรับค้นหา instance ของนักศึกษาใน student_list
+def search_student_by_id(student_id):
+    pass
+
+# TODO 3 : function สำหรับสร้างการลงทะเบียน โดยรับ instance ของ student และ subject
+def enroll_to_subject(student, subject):
+    if  isinstance(student,Student) and isinstance(subject, Subject):
+        if len(enrollment_list) == 0:
+            enrollment_list.append(Enrollment(subject, student))
+            return "Done"
+        
+        for x in enrollment_list:
+            if x.get_student() != student or x.get_subject() != subject:
+                enrollment_list.append(Enrollment(subject, student))
+                return "Done"
+            else:
+                return "Error"
+    else:
+        return"Error"
+
+# TODO 4 : function สำหรับลบการลงทะเบียน โดยรับ instance ของ student และ subject
+def drop_from_subject(student, subject):
+    if  isinstance(student,Student) and isinstance(subject, Subject):
+        for x in enrollment_list:
+            if x.student == student and x.subject == subject:
+                enrollment_list.remove(Enrollment(subject, student))
+                return "Done"
+            else:
+                return "Not Found"
+    else:
+        return"Error"
+
+# TODO 5 : function สำหรับค้นหาการลงทะเบียน โดยรับ instance ของ student และ subject
+def search_enrollment_subject_student(subject, student):
+    pass
+
+# TODO 6 : function สำหรับค้นหาการลงทะเบียนในรายวิชา โดยรับ instance ของ subject
+def search_student_enroll_in_subject(subject):
+    filter_list=[]
+    for x in enrollment_list:
+        if x.get_subject() == subject:
+            filter_list.append(x)
+        
+
+    return filter_list
+
+# TODO 7 : function สำหรับค้นหาการลงทะเบียนของนักศึกษาว่ามีวิชาอะไรบ้าง โดยรับ instance ของ student
+def search_subject_that_student_enrolled(student):
+    pass
+
+# TODO 8 : function สำหรับใส่เกรดลงในการลงทะเบียน โดยรับ instance ของ student และ subject
+def assign_grade(student, subject, grade):
+    pass
+
+# TODO 9 : function สำหรับคืน instance ของอาจารย์ที่สอนในวิชา
+def get_teacher_teach(subject_search):
+    pass
+
+# TODO 10 : function สำหรับค้นหาจำนวนของนักศึกษาที่ลงทะเบียนในรายวิชา โดยรับ instance ของ subject
+def get_no_of_student_enrolled(subject):
+    pass
+
+# TODO 11 : function สำหรับค้นหาข้อมูลการลงทะเบียนและผลการเรียนโดยรับ instance ของ student
+# TODO : และ คืนค่าเป็น dictionary { ‘subject_id’ : [‘subject_name’, ‘grade’ }
+def get_student_record(student):
+    pass
+
+# แปลงจาก เกรด เป็นตัวเลข
+def grade_to_count(grade):
+    grade_mapping = {'A': 4, 'B': 3, 'C': 2, 'D': 1}
+    return grade_mapping.get(grade, 0)
+
+# TODO 12 : function สำหรับคำนวณเกรดเฉลี่ยของนักศึกษา โดยรับ instance ของ student
+def get_student_GPS(student):
+    pass
+
+# ค้นหานักศึกษาลงทะเบียน โดยรับเป็น รหัสวิชา และคืนค่าเป็น dictionary {รหัส นศ. : ชื่อ นศ.}
+def list_student_enrolled_in_subject(subject_id):
+    subject = search_subject_by_id(subject_id)
+    if subject is None:
+        return "Subject not found"
+    filter_student_list = search_student_enroll_in_subject(subject)
+    student_dict = {}
+    for enrollment in filter_student_list:
+        student_dict[enrollment.get_student().get_student_id()] = enrollment.get_student().get_student_name()
+    return student_dict
+
+# ค้นหาวิชาที่นักศึกษาลงทะเบียน โดยรับเป็น รหัสนักศึกษา และคืนค่าเป็น dictionary {รหัสวิชา : ชื่อวิชา }
+def list_subject_enrolled_by_student(student_id):
+    student = search_student_by_id(student_id)
+    if student is None:
+        return "Student not found"
+    filter_subject_list = search_subject_that_student_enrolled(student)
+    subject_dict = {}
+    for enrollment in filter_subject_list:
+        subject_dict[enrollment.subject.subject_id] = enrollment.subject.subject_name
+    return subject_dict
+
+#######################################################################################
+
+#สร้าง instance พื้นฐาน
+def create_instance():
+    student_list.append(Student('66010001', "Keanu Welsh"))
+    student_list.append(Student('66010002', "Khadijah Burton"))
+    student_list.append(Student('66010003', "Jean Caldwell"))
+    student_list.append(Student('66010004', "Jayden Mccall"))
+    student_list.append(Student('66010005', "Owain Johnston"))
+    student_list.append(Student('66010006', "Isra Cabrera"))
+    student_list.append(Student('66010007', "Frances Haynes"))
+    student_list.append(Student('66010008', "Steven Moore"))
+    student_list.append(Student('66010009', "Zoe Juarez"))
+    student_list.append(Student('66010010', "Sebastien Golden"))
+
+    subject_list.append(Subject('CS101', "Computer Programming 1", 3))
+    subject_list.append(Subject('CS102', "Computer Programming 2", 3))
+    subject_list.append(Subject('CS103', "Data Structure", 3))
+
+    teacher_list.append(Teacher('T001', "Mr. Welsh"))
+    teacher_list.append(Teacher('T002', "Mr. Burton"))
+    teacher_list.append(Teacher('T003', "Mr. Smith"))
+
+    subject_list[0].assign_teacher(teacher_list[0])
+    subject_list[1].assign_teacher(teacher_list[1])
+    subject_list[2].assign_teacher(teacher_list[2])
+
+# ลงทะเบียน
+def register():
+    enroll_to_subject(student_list[0], subject_list[0])  # 001 -> CS101
+    enroll_to_subject(student_list[0], subject_list[1])  # 001 -> CS102
+    enroll_to_subject(student_list[0], subject_list[2])  # 001 -> CS103
+    enroll_to_subject(student_list[1], subject_list[0])  # 002 -> CS101
+    enroll_to_subject(student_list[1], subject_list[1])  # 002 -> CS102
+    enroll_to_subject(student_list[1], subject_list[2])  # 002 -> CS103
+    enroll_to_subject(student_list[2], subject_list[0])  # 003 -> CS101
+    enroll_to_subject(student_list[2], subject_list[1])  # 003 -> CS102
+    enroll_to_subject(student_list[2], subject_list[2])  # 003 -> CS103
+    enroll_to_subject(student_list[3], subject_list[0])  # 004 -> CS101
+    enroll_to_subject(student_list[3], subject_list[1])  # 004 -> CS102
+    enroll_to_subject(student_list[4], subject_list[0])  # 005 -> CS101
+    enroll_to_subject(student_list[4], subject_list[2])  # 005 -> CS103
+    enroll_to_subject(student_list[5], subject_list[1])  # 006 -> CS102
+    enroll_to_subject(student_list[5], subject_list[2])  # 006 -> CS103
+    enroll_to_subject(student_list[6], subject_list[0])  # 007 -> CS101
+    enroll_to_subject(student_list[7], subject_list[1])  # 008 -> CS102
+    enroll_to_subject(student_list[8], subject_list[2])  # 009 -> CS103
+
+
+create_instance()
+register()
+
+### Test Case #1 : test enroll_to_subject complete ###
+student_enroll = list_student_enrolled_in_subject('CS101')
+print("Test Case #1 : test enroll_to_subject complete")
+print("Answer : {'66010001': 'Keanu Welsh', '66010002': 'Khadijah Burton', '66010003': 'Jean Caldwell', '66010004': 'Jayden Mccall', '66010005': 'Owain Johnston', '66010007': 'Frances Haynes'}")
+print(student_enroll)
+print("")
+
+# ### Test case #2 : test enroll_to_subject in case of invalid argument
+# print("Test case #2 : test enroll_to_subject in case of invalid argument")
+# print("Answer : Error")
+# print(enroll_to_subject('66010001','CS101'))
+# print("")
+
+
+
+    
+# print("Student List---------------------------------")
+# for x in student_list:
+#     print(x)
+# print("Subject List---------------------------------")
+# for x in subject_list:
+#     print(x)
+# print("Teacher List---------------------------------")
+# for x in teacher_list:
+#     print(x)
+
+
+# subject = search_subject_by_id('CS101')
+# filter_student_list = search_student_enroll_in_subject(subject)
+# for x in filter_student_list:
+#     print(x.get_student_name(), x.get_student_id())
